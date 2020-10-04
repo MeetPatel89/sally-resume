7a1b4832ec1d5a1af45950b7fae92e9d92183d76
Meet Patel <meetaiims@yahoo.com>
2020-10-04 06:28:07 -0400

    Updates

---
+++ precache-manifest.7816a55bc57e1043c4bd6c10efe920c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f1c7f96e5bb45ab06999f327c6b1b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90b03f0795ef7e40114d",
+    "url": "/static/js/2.58467df8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58467df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd06331ae805c7d82009",
+    "url": "/static/js/main.ca24f4bd.chunk.js"
+  },
+  {
+    "revision": "d2ea1d8ebf871b35014c",
+    "url": "/static/js/runtime-main.21f7a303.js"
+  }
+]);
